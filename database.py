4bf154a0d8117d48078eb0744ab1fffaa41bf6cc
Ryan Belanger <rbelange@uwaterloo.ca>
2024-01-11 10:51:05 -0500

    initial

---
+++ database.py
@@ -0,0 +1,3 @@
+# This file will handle the setting up of the databse - will be its own script
+# Setups up DB
+
---
+++ main.py
@@ -0,0 +1,13 @@
+# This is the main file of the hydrotronics project.
+# Running this Python file on the PI will cause the project to run
+# May look at making a compiled binary of final version
+
+
+
+
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
