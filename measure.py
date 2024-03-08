bdb62da2894b82c4d408a3dcce2cd9fbee3cc152
ryam <rbelange@uwaterloo.ca>
2024-03-08 00:47:47 +0000

    udpate

--- measure.py
+++ measure.py
@@ -9,16 +9,20 @@ from time import sleep
 # camera.resolution = (1024, 768)
 # camera.start_preview()
 target_tds = 500
-growth = 10
+growth = 0
 def determine_target_tds(elapsed_time):
     #get_photo()
     if elapsed_time > 1814400:
         target_tds = 600 #subject to change
+        growth = 80
     elif elapsed_time > 1209600:
         target_tds = 550 #subject to change
+        growth = 60
     elif elapsed_time > 604800:
+        growth = 40
         target_tds = 500 #subject to change
     else:
+        growth = 20
         target_tds = 500 #subject to change
     return target_tds, growth
 
